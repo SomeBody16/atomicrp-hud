09e2ebf390386e97c6b4ea9cb82e251a8872da10
Filip <filip.nowakowski2000@outlook.com>
2021-09-01 19:55:22 +0200

    Everything is bigger

--- html/script.js
+++ html/script.js
@@ -10,13 +10,13 @@ const shadow = {
   shadowOpacity: 0.7,
 };
 
-const mapRadius = stage.height() * 0.42;
+const mapRadius = stage.height() * 0.40;
 const mapPos = {
-  x: mapRadius + (stage.width() * 0.04),
+  x: mapRadius + (stage.width() * 0.045),
   y: (stage.height() * 0.975) - mapRadius,
 }
-const statsRadius = mapRadius + 50;
-const textRadius = statsRadius + 50;
+const statsRadius = mapRadius + 65;
+const textRadius = statsRadius + 65;
 
 const initCompass = () => {
   const group = new Konva.Group();
@@ -120,7 +120,7 @@ const initCompass = () => {
   return group;
 }
 
-const getStatPosition = (order) => getCirclePos(statsRadius, 235 - (order * 13), mapPos);
+const getStatPosition = (order) => getCirclePos(statsRadius, 230 - (order * 15.5), mapPos);
 const initStats = () => {
   const stats = {
     health: {
@@ -146,7 +146,7 @@ const initStats = () => {
       color: '#0FF',
       value: 0.75,
       icon: 'M205.22 22.09c-7.94-28.78-49.44-30.12-58.44 0C100.01 179.85 0 222.72 0 333.91 0 432.35 78.72 512 176 512s176-79.65 176-178.09c0-111.75-99.79-153.34-146.78-311.82zM176 448c-61.75 0-112-50.25-112-112 0-8.84 7.16-16 16-16s16 7.16 16 16c0 44.11 35.89 80 80 80 8.84 0 16 7.16 16 16s-7.16 16-16 16z',
-      iconMargin: 5,
+      iconMargin: 7,
     },
     voice: {
       order: 4,
@@ -159,7 +159,7 @@ const initStats = () => {
       color: '#C8BFE7',
       value: 0.75,
       icon: 'M636.11 390.15C614.44 308.85 580.07 231 534.1 159.13 511.98 124.56 498.03 96 454.05 96 415.36 96 384 125.42 384 161.71v60.11l-32.88-21.92a15.996 15.996 0 0 1-7.12-13.31V16c0-8.84-7.16-16-16-16h-16c-8.84 0-16 7.16-16 16v170.59c0 5.35-2.67 10.34-7.12 13.31L256 221.82v-60.11C256 125.42 224.64 96 185.95 96c-43.98 0-57.93 28.56-80.05 63.13C59.93 231 25.56 308.85 3.89 390.15 1.3 399.84 0 409.79 0 419.78c0 61.23 62.48 105.44 125.24 88.62l59.5-15.95c42.18-11.3 71.26-47.47 71.26-88.62v-87.49l-85.84 57.23a7.992 7.992 0 0 1-11.09-2.22l-8.88-13.31a7.992 7.992 0 0 1 2.22-11.09L320 235.23l167.59 111.72a7.994 7.994 0 0 1 2.22 11.09l-8.88 13.31a7.994 7.994 0 0 1-11.09 2.22L384 316.34v87.49c0 41.15 29.08 77.31 71.26 88.62l59.5 15.95C577.52 525.22 640 481.01 640 419.78c0-9.99-1.3-19.94-3.89-29.63z',
-      iconMargin: -2,
+      iconMargin: -5,
       iconScale: 0.95,
       hidden: true,
     },
@@ -168,7 +168,7 @@ const initStats = () => {
       color: '#7B7',
       value: 0.75,
       icon: 'M143.25 220.81l-12.42 46.37c-3.01 11.25-3.63 22.89-2.41 34.39l-35.2 28.98c-6.57 5.41-16.31-.43-14.62-8.77l15.44-76.68c1.06-5.26-2.66-10.28-8-10.79l-77.86-7.55c-8.47-.82-11.23-11.83-4.14-16.54l65.15-43.3c4.46-2.97 5.38-9.15 1.98-13.29L21.46 93.22c-5.41-6.57.43-16.3 8.78-14.62l76.68 15.44c5.26 1.06 10.28-2.66 10.8-8l7.55-77.86c.82-8.48 11.83-11.23 16.55-4.14l43.3 65.14c2.97 4.46 9.15 5.38 13.29 1.98l60.4-49.71c6.57-5.41 16.3.43 14.62 8.77L262.1 86.38c-2.71 3.05-5.43 6.09-7.91 9.4l-32.15 42.97-10.71 14.32c-32.73 8.76-59.18 34.53-68.08 67.74zm494.57 132.51l-12.42 46.36c-3.13 11.68-9.38 21.61-17.55 29.36a66.876 66.876 0 0 1-8.76 7l-13.99 52.23c-1.14 4.27-3.1 8.1-5.65 11.38-7.67 9.84-20.74 14.68-33.54 11.25L515 502.62c-17.07-4.57-27.2-22.12-22.63-39.19l8.28-30.91-247.28-66.26-8.28 30.91c-4.57 17.07-22.12 27.2-39.19 22.63l-30.91-8.28c-12.8-3.43-21.7-14.16-23.42-26.51-.57-4.12-.35-8.42.79-12.68l13.99-52.23a66.62 66.62 0 0 1-4.09-10.45c-3.2-10.79-3.65-22.52-.52-34.2l12.42-46.37c5.31-19.8 19.36-34.83 36.89-42.21a64.336 64.336 0 0 1 18.49-4.72l18.13-24.23 32.15-42.97c3.45-4.61 7.19-8.9 11.2-12.84 8-7.89 17.03-14.44 26.74-19.51 4.86-2.54 9.89-4.71 15.05-6.49 10.33-3.58 21.19-5.63 32.24-6.04 11.05-.41 22.31.82 33.43 3.8l122.68 32.87c11.12 2.98 21.48 7.54 30.85 13.43a111.11 111.11 0 0 1 34.69 34.5c8.82 13.88 14.64 29.84 16.68 46.99l6.36 53.29 3.59 30.05a64.49 64.49 0 0 1 22.74 29.93c4.39 11.88 5.29 25.19 1.75 38.39zM255.58 234.34c-18.55-4.97-34.21 4.04-39.17 22.53-4.96 18.49 4.11 34.12 22.65 39.09 18.55 4.97 45.54 15.51 50.49-2.98 4.96-18.49-15.43-53.67-33.97-58.64zm290.61 28.17l-6.36-53.29c-.58-4.87-1.89-9.53-3.82-13.86-5.8-12.99-17.2-23.01-31.42-26.82l-122.68-32.87a48.008 48.008 0 0 0-50.86 17.61l-32.15 42.97 172 46.08 75.29 20.18zm18.49 54.65c-18.55-4.97-53.8 15.31-58.75 33.79-4.95 18.49 23.69 22.86 42.24 27.83 18.55 4.97 34.21-4.04 39.17-22.53 4.95-18.48-4.11-34.12-22.66-39.09z',
-      iconMargin: -3,
+      iconMargin: -6,
       hidden: true,
     },
     carFuel: {
@@ -186,15 +186,15 @@ const initStats = () => {
 
     const background = new Konva.Circle({
       ...pos,
-      radius: 30,
+      radius: 40,
       fill: 'black',
       stroke: pSBC(-0.75, stat.color),
       ...shadow,
     });
     const value = new Konva.Arc({
       ...pos,
-      innerRadius: 27,
-      outerRadius: 33,
+      innerRadius: 35,
+      outerRadius: 45,
       angle: stat.value * 360,
       fill: stat.color,
       ...shadow,
@@ -202,13 +202,13 @@ const initStats = () => {
     value.rotation(-90);
 
     const icon = new Konva.Path({
-      x: pos.x - 15 + (stat.iconMargin ?? 0),
-      y: pos.y - 15,
+      x: pos.x - 22 + (stat.iconMargin ?? 0),
+      y: pos.y - 22,
       data: stat.icon,
       fill: 'lightgray',
       scale: {
-        x: 0.06 * (stat.iconScale ?? 1),
-        y: 0.06 * (stat.iconScale ?? 1),
+        x: 0.09 * (stat.iconScale ?? 1),
+        y: 0.09 * (stat.iconScale ?? 1),
       },
     });
 
@@ -243,8 +243,8 @@ const initStats = () => {
         background.position(pos);
         value.position(pos);
         icon.position({
-          x: pos.x - 15 + (stat.iconMargin ?? 0),
-          y: pos.y - 15,
+          x: pos.x - 22 + (stat.iconMargin ?? 0),
+          y: pos.y - 22,
         });
       },
       moveTo: (pos, duration = 0.5) => {
@@ -252,8 +252,8 @@ const initStats = () => {
         new Konva.Tween({ node: value, ...pos, duration }).play();
         new Konva.Tween({
           node: icon,
-          x: pos.x - 15 + (stat.iconMargin ?? 0),
-          y: pos.y - 15,
+          x: pos.x - 22 + (stat.iconMargin ?? 0),
+          y: pos.y - 22,
           duration
         }).play();
       },
@@ -287,17 +287,17 @@ const initAtomicId = () => {
 
 const initPlace = () => {
   const primary = new Konva.Text({
-    ...getCirclePos(textRadius, 153, mapPos),
+    ...getCirclePos(textRadius, 147, mapPos),
     text: `Pillbox Hill`,
-    fontSize: 40,
+    fontSize: 50,
     fontFamily: 'Redemption',
     fill: '#F60',
     ...shadow,
   });
   const secondary = new Konva.Text({
-    ...getCirclePos(textRadius, 142, mapPos),
+    ...getCirclePos(textRadius, 135, mapPos),
     text: `Elgin Ave`,
-    fontSize: 30,
+    fontSize: 40,
     fontFamily: 'Redemption',
     fill: '#DDD',
     ...shadow,
@@ -306,31 +306,32 @@ const initPlace = () => {
   return { primary, secondary };
 }
 
+const carMaxAngle = 288;
 const initCar = () => {
   const speedometer = new Konva.Arc({
     ...mapPos,
     innerRadius: statsRadius + 5,
     outerRadius: statsRadius + 25,
-    angle: 260,
+    angle: carMaxAngle,
     fill: '#FA0',
     clockwise: true,
     ...shadow,
   });
-  speedometer.rotation(55);
+  speedometer.rotation(50);
 
   const tachometer = new Konva.Arc({
     ...mapPos,
     innerRadius: statsRadius - 5,
     outerRadius: statsRadius - 15,
-    angle: 260,
-      fill: '#888',
+    angle: carMaxAngle,
+    fill: '#888',
     clockwise: true,
     ...shadow,
   });
-  tachometer.rotation(55);
+  tachometer.rotation(50);
 
   const speedometerText = new Konva.Text({
-    ...getCirclePos(textRadius, 50, mapPos),
+    ...getCirclePos(textRadius, 56, mapPos),
     text: `0mph`,
     fontSize: 50,
     fontFamily: 'Redemption',
@@ -431,8 +432,8 @@ const hudManager = {
   },
   car: {
     update: (speed, turnover, speedText) => {
-      new Konva.Tween({node: hud.component.car.speedometer, duration: 0.1, angle: 360 - (speed * 100)}).play();
-      new Konva.Tween({node: hud.component.car.tachometer, duration: 0.1, angle: 360 - (turnover * 100)}).play();
+      new Konva.Tween({node: hud.component.car.speedometer, duration: 0.1, angle: 360 - (speed * (360 - carMaxAngle))}).play();
+      new Konva.Tween({node: hud.component.car.tachometer, duration: 0.1, angle: 360 - (turnover * (360 - carMaxAngle))}).play();
       hud.component.car.speedometerText.text(speedText);
     },
   },
@@ -469,7 +470,7 @@ const hudManager = {
         }
         const pos = !hud.switch.compassOpened
           ? getStatPosition(stat.order + orderDiff)
-          : {...getStatPosition(0), y: stage.height() - 140 - ((stat.order + orderDiff) * 80)}
+          : {...getStatPosition(0), y: stage.height() - 140 - ((stat.order + orderDiff) * 100)}
         stat.moveTo(pos);
       }
     }
@@ -535,5 +536,5 @@ const hudManager = {
 
 hudManager._initStage();
 hudManager._draw();
-setTimeout(hudManager.setMode.onFoot, 500);
+// setTimeout(hudManager.setMode.onFoot, 500);
 setTimeout(hudManager._draw, 1000);
